4297e3fc8b8ab913299736674b7bbd205dc2e0e1
Srinivas Joshi <srinivasjoshi60@gmail.com>
2023-03-09 14:49:16 +0530

    base setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+
+		// Or if using `src` directory:
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				// prettier-ignore
+				'purple': '#2F2E41',
+				// prettier-ignore
+				'orange': '#FFA07A',
+			},
+		},
+	},
+	plugins: [],
+};
